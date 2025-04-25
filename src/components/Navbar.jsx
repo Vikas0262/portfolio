41df190d2c19d3922292d1dd6d59fdb672429bdb
vikas0262 <vikas.v6543@gmail.com>
2025-04-25 14:39:50 +0530

    make working mobile navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -88,7 +88,7 @@ function Navbar() {
         >
           <ul className="flex items-center justify-around gap-8 w-[90%] font-fira text-slate-50 text-lg">
             {/* Home Link */}
-            <a href="#Home">
+            <a href="#">
               <li className="flex flex-col items-center justify-center gap-1 cursor-pointer">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                   <path d="M22 10.498c0-1.21-.81-2.76-1.8-3.45l-6.18-4.33c-1.4-.98-3.65-.93-5 .12l-5.39 4.2c-.9.7-1.63 2.19-1.63 3.32v7.41c0 2.32 1.89 4.22 4.21 4.22h11.58c2.32 0 4.21-1.9 4.21-4.21v-3.1M12 17.988v-3" stroke="#d9e3f0" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
@@ -98,7 +98,7 @@ function Navbar() {
             </a>
 
             {/* Skills Link */}
-            <a href="#Skills">
+            <a href="#skills">
               <li className="flex flex-col items-center justify-center gap-1 cursor-pointer">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                   <path d="M6.89 9c.98.49 1.82 1.23 2.43 2.15.35.52.35 1.19 0 1.71-.61.91-1.45 1.65-2.43 2.14" stroke="#d9e3f0" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
@@ -109,7 +109,7 @@ function Navbar() {
             </a>
 
             {/* Projects Link */}
-            <a href="#Projects">
+            <a href="#projects">
               <li className="flex flex-col items-center justify-center gap-1 cursor-pointer">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                   <path d="M12 2v20M2 9.5h10M12 14.5h10M3.96 3.32C2.6 4.43 2 6.27 2 9v6c0 5 2 7 7 7h6c5 0 7-2 7-7V9c0-5-2-7-7-7H9" stroke="#d9e3f0" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
@@ -118,13 +118,13 @@ function Navbar() {
               </li>
             </a>
 
-            {/* About Link */}
-            <a href="#About">
+            {/* contact Link */}
+            <a href="#contact">
               <li className="flex flex-col items-center justify-center gap-1 cursor-pointer">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                   <path d="M15.68 3.96c.48.71.76 1.56.76 2.48-.01 2.4-1.9 4.35-4.28 4.43-.1-.01-.22-.01-.33 0-2.21-.07-4-1.76-4.24-3.92C7.3 4.38 9.41 2 11.99 2M6.99 14.56c-2.42 1.62-2.42 4.26 0 5.87 2.75 1.84 7.26 1.84 10.01 0 2.42-1.62 2.42-4.26 0-5.87-2.73-1.83-7.24-1.83-10.01 0Z" stroke="#d9e3f0" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
                 </svg>
-                <h2 className="text-sm font-normal font-ubuntu">About</h2>
+                <h2 className="text-sm font-normal font-ubuntu">Contact</h2>
               </li>
             </a>
           </ul>
