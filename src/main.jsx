6cfa182386e3883e6baad9ea32128af4a5eaf01c
vikas0262 <vikas.v6543@gmail.com>
2025-06-22 10:27:38 +0530

    fix: re-enable HelmetProvider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+if (typeof window !== "undefined") {
+  window.scrollTo(0, 0);
+}
+if (typeof document !== 'undefined') {
+  document.body.classList.add('dark');
+}
 
+if (navigator.userAgent === 'ReactSnap') {
+  window.snapSaveState = () => {
+    return new Promise(resolve => setTimeout(resolve, 200));
+  };
+}
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <HelmetProvider> */}
+      <App />
+    {/* </HelmetProvider> */}
   </StrictMode>,
 )
