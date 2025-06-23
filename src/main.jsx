2ef6d603d08abd9ed8fcdf22d216367aafa726ee
vikas0262 <vikas.v6543@gmail.com>
2025-06-24 00:36:58 +0530

    fix: remove commented code in main.jsx for cleaner codebase

--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,18 @@ import './index.css'
 import App from './App.jsx'
 import { HelmetProvider } from 'react-helmet-async';
 
-if (typeof window !== "undefined") {
-  window.scrollTo(0, 0);
-}
-if (typeof document !== 'undefined') {
-  document.body.classList.add('dark');
-}
+// if (typeof window !== "undefined") {
+//   window.scrollTo(0, 0);
+// }
+// if (typeof document !== 'undefined') {
+//   document.body.classList.add('dark');
+// }
 
-if (navigator.userAgent === 'ReactSnap') {
-  window.snapSaveState = () => {
-    return new Promise(resolve => setTimeout(resolve, 200));
-  };
-}
+// if (navigator.userAgent === 'ReactSnap') {
+//   window.snapSaveState = () => {
+//     return new Promise(resolve => setTimeout(resolve, 200));
+//   };
+// }
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HelmetProvider>
